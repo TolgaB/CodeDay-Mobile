fa88268e5d2092b21d5620185b5cc2348a674c31
Tolgab <Mathman290@gmail.com>
2015-08-17 21:21:27 -0700

    Front page

---
+++ CodeDay-Mobile/FrontPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrontPageViewController.h
+//  CodeDay-Mobile
+//
+//  Created by Tolga Beser on 8/17/15.
+//  Copyright (c) 2015 Tolga Beser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrontPageViewController : UIViewController
+
+@end
---
+++ CodeDay-Mobile/FrontPageViewController.m
@@ -0,0 +1,13 @@
+//
+//  FrontPageViewController.m
+//  CodeDay-Mobile
+//
+//  Created by Tolga Beser on 8/17/15.
+//  Copyright (c) 2015 Tolga Beser. All rights reserved.
+//
+
+#import "FrontPageViewController.h"
+
+@implementation FrontPageViewController
+
+@end
