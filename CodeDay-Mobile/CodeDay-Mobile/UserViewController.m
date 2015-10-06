7ef324eed2a1f00972087ec930cd840a33845706
Tolgab <Mathman290@gmail.com>
2015-10-06 10:31:01 -0700

    Worked on the userview

--- CodeDay-Mobile/CodeDay-Mobile/UserViewController.m
+++ CodeDay-Mobile/CodeDay-Mobile/UserViewController.m
@@ -7,7 +7,34 @@
 //
 
 #import "UserViewController.h"
+#import "AppCommunicate.h"
 
-@implementation UserViewController
+@interface UserViewController ()
+@property (nonatomic, strong)AppCommunicate *communicate;
+@end
+
+@implementation UserViewController {
+    
+    __weak IBOutlet UILabel *nameLabel;
+    NSString *userID;
+    __weak IBOutlet UILabel *emailLabel;
+    NSDictionary *userInfo;
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setUpUserValues];
+    _communicate = [[AppCommunicate alloc] init];
+}
+
+-(void)setUpUserValues {
+   userID = [[NSUserDefaults standardUserDefaults] objectForKey:@"userID"];
+    userInfo = [_communicate getRegisterInfo:userID];
+    NSLog(@"manual breakpoint");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
 
 @end
--- CodeDay-Mobile/DerivedData/CodeDay-Mobile/Build/Intermediates/CodeDay-Mobile.build/Debug-iphonesimulator/CodeDay-Mobile.build/Objects-normal/i386/UserViewController.d
+++ CodeDay-Mobile/DerivedData/CodeDay-Mobile/Build/Intermediates/CodeDay-Mobile.build/Debug-iphonesimulator/CodeDay-Mobile.build/Objects-normal/i386/UserViewController.d
@@ -1,3 +1,5 @@
 dependencies: \
   /Users/Tolga/Desktop/CodeDay-Mobile/CodeDay-Mobile/CodeDay-Mobile/UserViewController.m \
-  /Users/Tolga/Desktop/CodeDay-Mobile/CodeDay-Mobile/CodeDay-Mobile/UserViewController.h
+  /Users/Tolga/Desktop/CodeDay-Mobile/CodeDay-Mobile/CodeDay-Mobile/UserViewController.h \
+  /Users/Tolga/Desktop/CodeDay-Mobile/CodeDay-Mobile/CodeDay-Mobile/AppCommunicate.h \
+  /Users/Tolga/Desktop/CodeDay-Mobile/CodeDay-Mobile/CodeDay-Mobile/AppDelegate.h
